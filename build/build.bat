f944384c43f452780e6fb95e0e41a6f29ca05888
mamboer <mamboer@live.com>
2013-06-04 17:21:33 +0800

    move the released zip file to the nwapp repo

--- build/build.bat
+++ build/build.bat
@@ -5,7 +5,13 @@ set APP_VERSION=
 rem;;set APP_SRCFILES=..\css ..\js ..\img ..\fonts ..\about.html ..\index.html ..\package.json ..\setting.html ..\detail.html ..\icon.png
 set APP_SRCFILES=@files.txt
 set NW_FOLDER=..\..\nwapp\nw
+set NWAPP_FOLDER=..\..\nwapp\release
 set NW_BUILDFILES=%NW_FOLDER%\nwfiles.txt
 set TEMPFOLDER=_temp
 
 call %NW_FOLDER%\make.bat
+
+rem;; move the zip file to the NWAPP_FOLDER
+move "%APP_NAME%%APP_VERSION%.zip" %NWAPP_FOLDER%
+
+pause
